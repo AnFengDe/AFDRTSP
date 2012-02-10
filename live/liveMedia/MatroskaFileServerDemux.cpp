45da1061a9cb91d259d7e48b47c30ea2c1cec9a1
田朝晖 <chtian@anfengde.com>
2012-02-10 19:24:07 +0800

    remove H264 file

--- live/liveMedia/MatroskaFileServerDemux.cpp
+++ live/liveMedia/MatroskaFileServerDemux.cpp
@@ -23,7 +23,6 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 #include "AACAudioMatroskaFileServerMediaSubsession.hh"
 #include "AC3AudioMatroskaFileServerMediaSubsession.hh"
 #include "VorbisAudioMatroskaFileServerMediaSubsession.hh"
-#include "H264VideoMatroskaFileServerMediaSubsession.hh"
 #include "VP8VideoMatroskaFileServerMediaSubsession.hh"
 #include "T140TextMatroskaFileServerMediaSubsession.hh"
 
@@ -65,9 +64,6 @@ ServerMediaSubsession* MatroskaFileServerDemux::newServerMediaSubsession(unsigne
   } else if (strncmp(track->codecID, "A_VORBIS", 8) == 0) {
     track->mimeType = "audio/VORBIS";
     result = VorbisAudioMatroskaFileServerMediaSubsession::createNew(*this, track->trackNumber);
-  } else if (strcmp(track->codecID, "V_MPEG4/ISO/AVC") == 0) {
-    track->mimeType = "video/H264";
-    result = H264VideoMatroskaFileServerMediaSubsession::createNew(*this, track->trackNumber);
   } else if (strncmp(track->codecID, "V_VP8", 5) == 0) {
     track->mimeType = "video/VP8";
     result = VP8VideoMatroskaFileServerMediaSubsession::createNew(*this, track->trackNumber);
