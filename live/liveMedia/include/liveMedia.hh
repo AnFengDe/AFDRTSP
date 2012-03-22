0e621fc7bf97a3f54f410ffac3238e00074edf14
田朝晖 <chtian@anfengde.com>
2012-03-22 13:55:58 +0800

    remove Passive Server Media Subsession

--- live/liveMedia/include/liveMedia.hh
+++ live/liveMedia/include/liveMedia.hh
@@ -26,6 +26,5 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 
 #include "RTSPServerSupportingHTTPStreaming.hh"
 #include "RTSPClient.hh"
-#include "PassiveServerMediaSubsession.hh"
 
 #endif
