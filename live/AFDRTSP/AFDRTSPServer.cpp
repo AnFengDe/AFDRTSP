7e660931e0b8e33a5c8e7b113d9d7f92fdf97e64
田朝晖 <chtian@anfengde>
2012-03-25 17:48:35 +0800

    options 回调函数工作了

--- live/AFDRTSP/AFDRTSPServer.cpp
+++ live/AFDRTSP/AFDRTSPServer.cpp
@@ -98,58 +98,19 @@ extern "C" bool server_cleanup()
 
 extern "C" bool run_rtsp_srv(st_Handle_Cmd_Callback* pstCallback, unsigned short listen_port)
 {
+    if (NULL == pstCallback ) return false;
+        
     g_pstCallback = pstCallback;
-#if 0
-  // Create the RTSP server.  Try first with the default port number (554),
-  // and then with the alternative port number (8554):
-  portNumBits rtspServerPortNum = 554;
-  rtspServer = DynamicRTSPServer::createNew(*env, rtspServerPortNum, authDB);
-  if (rtspServer == NULL) {
-    rtspServerPortNum = 8554;
-    rtspServer = DynamicRTSPServer::createNew(*env, rtspServerPortNum, authDB);
-  }
-  if (rtspServer == NULL) {
-    *env << "Failed to create RTSP server: " << env->getResultMsg() << "\n";
-    exit(1);
-  }
+  
+    portNumBits server_port = listen_port;
+    g_server_rt->server = RTSPServer::createNew(*g_server_rt->env, server_port);
+    if ( NULL == g_server_rt->server)
+    {
+        *g_server_rt->env << "Failed to create RTSP server: " << *(g_server_rt->env)->getResultMsg() << "\n";
+        return false;
+    }
 
-  *env << "RTSP Media Server\n";
- /* *env << "\tversion " << MEDIA_SERVER_VERSION_STRING
-       << " (LIVE555 Streaming Media library version "
-       << LIVEMEDIA_LIBRARY_VERSION_STRING << ").\n";
+    *g_server_rt->env << "RTSP Media Server\n";
 
-  char* urlPrefix = rtspServer->rtspURLPrefix();
-  *env << "Play streams from this server using the URL\n\t"
-       << urlPrefix << "<filename>\nwhere <filename> is a file present in the current directory.\n";
-  *env << "Each file's type is inferred from its name suffix:\n";
-  *env << "\t\".264\" => a H.264 Video Elementary Stream file\n";
-  *env << "\t\".aac\" => an AAC Audio (ADTS format) file\n";
-  *env << "\t\".ac3\" => an AC-3 Audio file\n";
-  *env << "\t\".amr\" => an AMR Audio file\n";
-  *env << "\t\".dv\" => a DV Video file\n";
-  *env << "\t\".m4e\" => a MPEG-4 Video Elementary Stream file\n";
-  *env << "\t\".mkv\" => a Matroska audio+video+(optional)subtitles file\n";
-  *env << "\t\".mp3\" => a MPEG-1 or 2 Audio file\n";
-  *env << "\t\".mpg\" => a MPEG-1 or 2 Program Stream (audio+video) file\n";
-  *env << "\t\".ts\" => a MPEG Transport Stream file\n";
-  *env << "\t\t(a \".tsx\" index file - if present - provides server 'trick play' support)\n";
-  *env << "\t\".wav\" => a WAV Audio file\n";
-  *env << "\t\".webm\" => a WebM audio(Vorbis)+video(VP8) file\n";
-  *env << "See http://www.live555.com/mediaServer/ for additional documentation.\n";*/
-
-  // Also, attempt to create a HTTP server for RTSP-over-HTTP tunneling.
-  // Try first with the default HTTP port (80), and then with the alternative HTTP
-  // port numbers (8000 and 8080).
-
-  if (rtspServer->setUpTunnelingOverHTTP(80) || rtspServer->setUpTunnelingOverHTTP(8000) || rtspServer->setUpTunnelingOverHTTP(8080)) {
-    *env << "(We use port " << rtspServer->httpServerPortNum() << " for optional RTSP-over-HTTP tunneling, or for HTTP live streaming (for indexed Transport Stream files only).)\n";
-  } else {
-    *env << "(RTSP-over-HTTP tunneling is not available.)\n";
-  }
-
-  env->taskScheduler().doEventLoop(); // does not return
-
-  return 0; // only to prevent compiler warning
-#endif
-    return false;
+    return true;
 }
--- live/testProgs/testRTSPServer.cpp
+++ live/testProgs/testRTSPServer.cpp
@@ -1,7 +1,59 @@
 #include <stdio.h>
+#include <string.h>
+#include "AFDRTSPServer.h"
+
+#ifdef WIN32
+#define STD_CALLBACK    __stdcall
+#else
+#define STD_CALLBACK    
+#endif
+
+void STD_CALLBACK handle_Options(char* cmd_names)
+{
+    sprintf(cmd_names, "OPTIONS, DESCRIBE, SETUP, TEARDOWN, PLAY, PAUSE, GET_PARAMETER, SET_PARAMETER");
+}
 
 int main()
 {
-    printf("Hello, I am testRTSPServer\n");
+    char cmd[256];
+    st_Handle_Cmd_Callback cb;
+    
+    ::memset(&cb, 0x00, sizeof(cb));
+    cb.options = handle_Options;
+            
+    if (false == server_init())
+    {
+        printf("AFD RTSP Server init failure\n");
+        return -1;
+    }
+    if (false == run_rtsp_srv(&cb, 8554)) 
+    {
+        printf("AFD RTSP Server init failure\n");
+        return -1;
+    }
+
+    while(1) //input loop
+    {
+        ::memset(cmd, 0x00, 256);
+        printf(	"Please input command : \n -exit exit program\n  Please input:");
+        
+        gets(cmd);
+        
+        if (0 == ::memcmp(cmd, "exit", 4))
+        {
+            if (false == server_cleanup())
+            {
+                printf("AFD RTSP Server cleanup failure\n");
+                return -1;
+            }
+            break;
+        }
+        else
+        {
+            printf("input command error!\n");
+        }
+    }
+    
+    return 0;
 }
 
