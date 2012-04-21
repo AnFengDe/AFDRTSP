c5ad87392a56de2631ce3070dd6d8d01fe9ff42b
田朝晖 <chtian@anfengde>
2012-04-22 05:02:27 +0800

    reformat

--- live/liveMedia/RTSPClient.cpp
+++ live/liveMedia/RTSPClient.cpp
@@ -417,43 +417,48 @@ int RTSPClient::connectToServer(int socketNum, portNumBits remotePortNum) {
 }
 
 char*
-RTSPClient::createAuthenticatorString(char const* cmd, char const* url) {
-  Authenticator& auth = fCurrentAuthenticator; // alias, for brevity
-  if (auth.realm() != NULL && auth.username() != NULL && auth.password() != NULL) {
-    // We have a filled-in authenticator, so use it:
-    char* authenticatorStr;
-    if (auth.nonce() != NULL) { // Digest authentication
-      char const* const authFmt =
-        "Authorization: Digest username=\"%s\", realm=\"%s\", "
-        "nonce=\"%s\", uri=\"%s\", response=\"%s\"\r\n";
-      char const* response = auth.computeDigestResponse(cmd, url);
-      unsigned authBufSize = strlen(authFmt)
-        + strlen(auth.username()) + strlen(auth.realm())
-        + strlen(auth.nonce()) + strlen(url) + strlen(response);
-      authenticatorStr = new char[authBufSize];
-      sprintf(authenticatorStr, authFmt,
-              auth.username(), auth.realm(),
-              auth.nonce(), url, response);
-      auth.reclaimDigestResponse(response);
-    } else { // Basic authentication
-      char const* const authFmt = "Authorization: Basic %s\r\n";
+RTSPClient::createAuthenticatorString(char const* cmd, char const* url) 
+{
+    Authenticator& auth = fCurrentAuthenticator; // alias, for brevity
+    if (auth.realm() != NULL && auth.username() != NULL && auth.password() != NULL) 
+    {
+        // We have a filled-in authenticator, so use it:
+        char* authenticatorStr;
+        if (auth.nonce() != NULL) 
+        { // Digest authentication
+            char const* const authFmt =
+                                        "Authorization: Digest username=\"%s\", realm=\"%s\", "
+                                        "nonce=\"%s\", uri=\"%s\", response=\"%s\"\r\n";
+            char const* response = auth.computeDigestResponse(cmd, url);
+            unsigned authBufSize = strlen(authFmt)
+                                    + strlen(auth.username()) + strlen(auth.realm())
+                                    + strlen(auth.nonce()) + strlen(url) + strlen(response);
+            authenticatorStr = new char[authBufSize];
+            sprintf(authenticatorStr, authFmt,
+                    auth.username(), auth.realm(),
+                    auth.nonce(), url, response);
+            auth.reclaimDigestResponse(response);
+        } 
+        else 
+        { // Basic authentication
+            char const* const authFmt = "Authorization: Basic %s\r\n";
 
-      unsigned usernamePasswordLength = strlen(auth.username()) + 1 + strlen(auth.password());
-      char* usernamePassword = new char[usernamePasswordLength+1];
-      sprintf(usernamePassword, "%s:%s", auth.username(), auth.password());
+            unsigned usernamePasswordLength = strlen(auth.username()) + 1 + strlen(auth.password());
+            char* usernamePassword = new char[usernamePasswordLength+1];
+            sprintf(usernamePassword, "%s:%s", auth.username(), auth.password());
 
-      char* response = base64Encode(usernamePassword, usernamePasswordLength);
-      unsigned const authBufSize = strlen(authFmt) + strlen(response) + 1;
-      authenticatorStr = new char[authBufSize];
-      sprintf(authenticatorStr, authFmt, response);
-      delete[] response; delete[] usernamePassword;
+            char* response = base64Encode(usernamePassword, usernamePasswordLength);
+            unsigned const authBufSize = strlen(authFmt) + strlen(response) + 1;
+            authenticatorStr = new char[authBufSize];
+            sprintf(authenticatorStr, authFmt, response);
+            delete[] response; delete[] usernamePassword;
+        }
+
+        return authenticatorStr;
     }
 
-    return authenticatorStr;
-  }
-
-  // We don't have a (filled-in) authenticator.
-  return strDup("");
+    // We don't have a (filled-in) authenticator.
+    return strDup("");
 }
 
 static char* createSessionString(char const* sessionId) {
