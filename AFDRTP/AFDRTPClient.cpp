4ab6a473bda49d984316a0606306aa0c7fede91f
田朝晖 <chtian@anfengde.com>
2012-04-27 16:23:37 +0800

    add rtp test and src

---
+++ AFDRTP/AFDRTPClient.cpp
@@ -0,0 +1,15 @@
+/*!
+ * \author chtian
+ * \brief AFD RTP Client implementation	
+ * \file AFDRTPClient.cpp
+ * \version 1.0 
+ * \date 2012-04-25 23:37
+ * */
+
+#include <stdio.h>
+
+int testclient()
+{
+    printf("hello,world!\n");
+    return 0;
+}
---
+++ AFDRTP/AFDRTPServer.h
@@ -0,0 +1,11 @@
+/*!
+ * \author chtian
+ * \brief AFD RTP Server API definition	
+ * \file AFDRTPServer.h
+ * \version 1.0 
+ * \date 2012-04-25 23:36
+ * */
+#ifndef _AFDRTPSERVER_H_
+#define _AFDRTPSERVER_H_
+
+#endif
